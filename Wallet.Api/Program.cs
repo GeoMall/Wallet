fd88b6e78d2800566b80bd20ea422052a4b60898
GeorgeMallia <george.mallia@river.tech>
2025-10-15 08:29:55 +0200

    docker file updates

--- Wallet.Api/Program.cs
+++ Wallet.Api/Program.cs
@@ -28,6 +28,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddMemoryCache();
 
+builder.Services.Configure<IpRateLimitingConfig>(builder.Configuration.GetSection(IpRateLimitingConfig.SectionName));
 builder.Services.AddInMemoryRateLimiting();
 builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
 
